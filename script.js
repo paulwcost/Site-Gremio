568a82fa1893e3e1946d397c695efc036d0e4c5f
paulwcost <fernandescostanetopaulo@gmail.com>
2025-05-16 01:14:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -15,7 +15,26 @@ document.querySelector('.prev').addEventListener('click', () => {
 
 // Auto slide
 setInterval(() => {
-  document.querySelector('.next').click();
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
 }, 5000);
 
 
+//MENU
+
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  navToggle.classList.toggle('active');
+});
+
+// Fecha menu ao clicar em qualquer link do menu
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', () => {
+    navMenu.classList.remove('active');
+    navToggle.classList.remove('active');
+  });
+});
